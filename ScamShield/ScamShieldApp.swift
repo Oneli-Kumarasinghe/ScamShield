0aa6bcd8a0c9e0c68f9b80ecff3190360b71e43c
Oneli-Kumarasinghe <onelisaku@gmail.com>
2025-04-18 23:53:28 +0530

    Initial Commit

---
+++ ScamShield/ScamShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScamShieldApp.swift
+//  ScamShield
+//
+//  Created by Theniya Kumarasinghe on 2025-04-18.
+//
+
+import SwiftUI
+
+@main
+struct ScamShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
